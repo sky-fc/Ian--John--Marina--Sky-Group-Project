79fc0e138e8c46877335d242990567df2c04834a
John <johnpua@outlook.com>
2023-12-18 17:40:42 -0800

    Need end pointfor RNL

--- server/server.js
+++ server/server.js
@@ -2,3 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
+const sequelize = require('./config/sequelize.config');
+const app = express();
+
+//... Endpoints for the login and registration
+
+sequelize.sync().then(() => {
+    app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    });
+});
