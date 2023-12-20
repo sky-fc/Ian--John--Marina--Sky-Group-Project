b6b380b9e59cff2f728066ea1e7be7f6de476193
John <johnpua@outlook.com>
2023-12-19 16:33:29 -0800

    added comment

--- server/routes/User.routes.js
+++ server/routes/User.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const UserController = require('../controllers/UserController'); // Make sure the path is correct
+const UserController = require('../controllers/UserController');
 const router = express.Router();
 
 router.post('/register', UserController.register);
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const sequelize = require('./config/sequelize.config');
 const userRoutes = require('./routes/User.routes');
 const app = express();
-
+//.. updated version with User.routes.js holds the login and registration
 app.use(cors());
 app.use(express.json());
 
