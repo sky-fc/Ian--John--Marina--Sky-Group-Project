7d2b0aafb8b9450e6076a3a28120be509e823fe7
Sky Fuzell-Casey <skyfc.work@gmail.com>
2023-12-21 12:18:43 -0800

    added .env to gitignore

--- server/config/sequelize.config.js
+++ server/config/sequelize.config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 //... schema nae ideas, your user, your password
-const sequelize = new Sequelize('ideas', 'root', 'imcnabb1234', {
+const sequelize = new Sequelize('ideas', 'root', '!Poppi318!', {
 host: 'localhost',
 dialect: 'mysql'
 });
